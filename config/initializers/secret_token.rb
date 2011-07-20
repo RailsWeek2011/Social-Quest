bcf2d4f3c0e1e2f79a032034cbf35fccb13ef10d
Heinrich Schild <hcschild@gmail.com>
2011-07-20 16:56:28 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialQuest::Application.config.secret_token = '750d2dbd088b98f182c65b5b012905ea3fb04b639cfadd9f740c9c09d53577105a488bf5db8cbe1232769cc453584d36b0d55d580de9b4d6b6dd4c1e49458456'
